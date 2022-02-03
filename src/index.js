827db670fcb1de25362c19f922a78d41a390588f
Caroline <anacdolima@gmail.com>
2022-02-02 22:59:39 -0300

    feat: POST /login route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import joi from "joi"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 import bcrypt from "bcrypt"
+import { v4 as uuid } from "uuid" 
 
 dotenv.config()
 const server = express()
@@ -23,6 +24,11 @@ const userSchema = joi.object({
   password: joi.string().required()
 })
 
+const loginSchema = joi.object({
+  email: joi.string().required(),
+  password: joi.string().required()
+})
+
 
 /* SignUp route */
 server.post("/sign-up", async (req, res) => {
@@ -37,15 +43,41 @@ server.post("/sign-up", async (req, res) => {
     
     const emailRegistered = await usersCollection.findOne({email: user.email})
     if(emailRegistered)
-      return res.status(422).send("Email já cadastrado")
+      return res.status(409).send("Email já cadastrado")
     
-
     const password = bcrypt.hashSync(user.password, 10)
     await usersCollection.insertOne({...user, password})
 
     res.sendStatus(201)
   } catch (error) {
-    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+server.post("/login", async (req, res) => {
+  const user = req.body
+
+  const validation = loginSchema.validate(user)
+  if(validation.error)
+    return res.status(422).send("Todos os campos devem ser preenchidos")
+  
+
+  try{
+    const userRegistered = await db.collection("users").findOne({email: user.email})
+    
+    if(!userRegistered)
+      return res.status(401).send("Email e/ou senha incorretos")
+    
+    if(!bcrypt.compareSync(user.password, userRegistered.password))
+      return res.status(401).send("Email e/ou senha incorretos")
+    
+    const token = uuid()
+    await db.collection("sessions").insertOne({token, userId: userRegistered._id})
+
+    const name = userRegistered.name
+
+    res.send({token, name})
+  } catch {
     res.sendStatus(500);
   }
 })
