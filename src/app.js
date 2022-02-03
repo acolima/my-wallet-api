e125a08aa66d1af6fd0e92c038f8e6273294eb5f
Caroline <anacdolima@gmail.com>
2022-02-03 14:28:45 -0300

    feat: POST /add-income route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express, { json } from "express"
 import cors from "cors"
 import { signIn, signUp } from "./controllers/authController.js"
+import { addIncome } from "./controllers/registerController.js"
 
 const server = express()
 server.use(cors())
@@ -10,5 +11,8 @@ server.use(json())
 server.post("/sign-up", signUp)
 
 server.post("/login", signIn)
+
+/* Registers routes */
+server.post("/add-income", addIncome)
 
 server.listen(5000, () => console.log("Listening on port 5000"))
---
+++ src/controllers/registerController.js
@@ -0,0 +1,33 @@
+import joi from "joi"
+import db from "../db.js"
+
+/* Schemes */
+const registerScheme = joi.object({
+  amount: joi.string().required(),
+  description: joi.string().required(),
+  type: joi.string().valid('income', 'expense').required(),
+  date: joi.string()
+})
+
+export async function addIncome(req, res){
+  const { authorization } = req.headers
+  const token = authorization?.replace("Bearer ", "")
+  const income = req.body
+
+  const validation = registerScheme.validate(income)
+  if(validation.error)
+    return res.status(422).send("Todos os campos devem ser preenchidos")
+
+  try {
+    const user = await db.collection("sessions").findOne({token})
+
+    if(!user)
+      return res.sendStatus(401)
+
+    await db.collection("registers").insertOne({...income, userId: user.userId})
+    res.sendStatus(201)
+  } catch {[
+    res.sendStatus(500)
+  ]}
+  
+}
