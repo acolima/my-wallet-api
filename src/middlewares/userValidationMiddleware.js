6d78037d4e984485b6bb3d4eec791cabde14af6a
Caroline <anacdolima@gmail.com>
2022-02-06 16:33:29 -0300

    fix: password validation on signup

--- src/middlewares/userValidationMiddleware.js
+++ src/middlewares/userValidationMiddleware.js
@@ -4,7 +4,7 @@ import { stripHtml } from "string-strip-html"
 const userSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().required(),
-  password: joi.string().required()
+  password: joi.string().required().min(6)
 })
 
 export default function userValidationMiddleware(req, res, next){
