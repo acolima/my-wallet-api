3be514b1aa4f23018791bfd19b2ea670686fecb3
Caroline <anacdolima@gmail.com>
2022-02-17 22:10:43 -0300

    gif for readme

--- src/controllers/recordController.js
+++ src/controllers/recordController.js
@@ -1,12 +1,11 @@
 import { ObjectId } from "mongodb"
 import db from "../db.js"
 
-export async function addRecord(req, res){
-  const { record } = res.locals
-  const { session } = res.locals
+export async function addRecord(req, res) {
+  const { record, session } = res.locals
 
-  try{
-    await db.collection("records").insertOne({...record, userId: session.userId})
+  try {
+    await db.collection("records").insertOne({ ...record, userId: session.userId })
 
     res.sendStatus(201)
   } catch {
@@ -14,23 +13,23 @@ export async function addRecord(req, res){
   }
 }
 
-export async function getRecord(req, res){
+export async function getRecord(req, res) {
   const { session } = res.locals
 
-  try{
-    const records = await db.collection("records").find({userId: session.userId}).toArray()
+  try {
+    const records = await db.collection("records").find({ userId: session.userId }).toArray()
 
     res.status(200).send(records)
   } catch {
     res.sendStatus(500)
-  }  
+  }
 }
 
-export async function deleteRecord(req, res){
+export async function deleteRecord(req, res) {
   const { id } = req.params
-  
+
   try {
-    await db.collection("records").deleteOne({_id: ObjectId(id)})
+    await db.collection("records").deleteOne({ _id: ObjectId(id) })
 
     res.sendStatus(200)
   } catch {
@@ -38,15 +37,15 @@ export async function deleteRecord(req, res){
   }
 }
 
-export async function updateRecord(req, res){
+export async function updateRecord(req, res) {
   const { id } = req.params
   const { record } = res.locals
 
   try {
-    const toUpdateRecord = await db.collection("records").findOne({_id: ObjectId(id)})
-    
-    if(toUpdateRecord){
-      await db.collection("records").updateOne({_id: ObjectId(id)}, {$set: {...record}})
+    const toUpdateRecord = await db.collection("records").findOne({ _id: ObjectId(id) })
+
+    if (toUpdateRecord) {
+      await db.collection("records").updateOne({ _id: ObjectId(id) }, { $set: { ...record } })
       res.sendStatus(200)
     }
   } catch {
