852b07b2e50d94749842ca03d327b6c9ea4b6fce
Caroline <anacdolima@gmail.com>
2022-02-17 22:05:11 -0300

    add .env.example file

--- src/middlewares/authValidationMiddleware.js
+++ src/middlewares/authValidationMiddleware.js
@@ -1,20 +1,20 @@
 import joi from "joi"
+import { stripHtml } from "string-strip-html"
 
 const loginSchema = joi.object({
   email: joi.string().required(),
   password: joi.string().required()
 })
 
-export default function authValidationMiddleware(req, res, next){
+export default function authValidationMiddleware(req, res, next) {
   const user = {
-    email: req.body.email,
-    password: req.body.password
+    email: stripHtml(req.body.email).result.trim(),
+    password: stripHtml(req.body.password).result.trim()
   }
   const validation = loginSchema.validate(user)
-  if(validation.error)
+  if (validation.error)
     return res.status(422).send("Todos os campos devem ser preenchidos")
-  
+
   res.locals.user = user
   next()
 }
-  
